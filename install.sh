ca69afbc6cc1fafc7b62f26a81a08108118093c8
NAS油条 <sfsyt233@outlook.com>
2026-07-22 21:31:53 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ if [[ -n $PREFIX ]]; then arch=aarch64;else [[ $arch == "x86_64" ]] && arch="amd
 echo "正在获取最新版本信息";tag_name=$(curl -s 'https://api.github.com/repos/nasyt233/nweb/releases/latest' | grep -m1 '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 curl -s https://myip.ipip.net | grep -oE "中国|China" >/dev/null 2>&1 && speed=https://gh-proxy.org/
 dow_url="${speed}https://github.com/nasyt233/nweb/releases/download/$tag_name/$tag_name-$myuname-$arch"
-echo "正在下载文件";curl --progress-bar -o nweb -L $dow_url || echo "文件下载失败"
+echo "正在下载文件";curl --progress-bar -o nweb -L $dow_url;if [[ $? -ne 0 ]]; then echo "文件下载失败,错误代码 $?";exit 1;fi
 chmod +x nweb;mv nweb $bin
 echo "nweb安装完成";echo "输入nweb查看帮助"
